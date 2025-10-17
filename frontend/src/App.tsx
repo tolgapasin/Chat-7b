4d703bef299727600b45d080a0dec17cf8d2a9ef
tolgapasin <tolgapasin@gmail.com>
2025-10-17 15:31:27 +1000

    Some improvements...

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,26 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import LoadingSpinner from "./components/LoadingSpinner";
+import type { SocketMessage } from "./models/SocketMessage.model";
 
-// https://www.geeksforgeeks.org/reactjs/real-time-updates-with-websockets-and-react-hooks/
 function App() {
-  //const [count, setCount] = useState(0);
-
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState<SocketMessage[]>([]);
   const [ws, setWs] = useState(null);
   const [message, setMessage] = useState("");
-  const [clientId, setClientId] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const websocket = new WebSocket("ws://127.0.0.1:8000/chat-socket");
 
+    // TODO: show on screen connection is successful and if it cuts out
     websocket.onopen = () => {
       console.log("WebSocket is connected");
-      // Generate a unique client ID
-      const id = Math.floor(Math.random() * 1000);
-      setClientId(id);
     };
 
     websocket.onmessage = (evt) => {
-      const message = evt.data;
+      const message = evt.data as SocketMessage;
       setMessages((prevMessages) => [...prevMessages, message]);
+      console.log(message.payload);
     };
 
     websocket.onclose = () => {
@@ -42,9 +40,9 @@ function App() {
         JSON.stringify({
           type: "message",
           payload: message,
-          clientId: clientId,
         })
       );
+      setIsLoading(true);
       setMessages((prevMessages) => [...prevMessages, message]);
       setMessage("");
     }
@@ -57,18 +55,18 @@ function App() {
   return (
     <>
       <h1>Chat 7b</h1>
-
       {messages.map((message, index) => (
         <p key={index}>{message}</p>
       ))}
-
+      {/* TODO: fix this */}
+      <LoadingSpinner isLoading={isLoading} />
       <div className="card">
         <p>How can I be of assistance?</p>
         <input
           type="text"
           placeholder="Ask away"
-          onFocus="this.placeholder"
-          onBlur="this.placeholder='Ask away'"
+          // onFocus="this.placeholder"
+          // onBlur="this.placeholder='Ask away'"
           value={message}
           onChange={handleInputChange}
           onKeyDown={(e) => {
---
+++ frontend/src/components/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+const LoadingSpinner = ({ isLoading }: { isLoading: boolean }) => {
+  if (isLoading) {
+    <div>LoadingSpinner</div>;
+  }
+
+  return null;
+};
+
+export default LoadingSpinner;
